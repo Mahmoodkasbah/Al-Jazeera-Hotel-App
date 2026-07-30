d8a9fee28aadfc5955d3a21e50fe08501430feda
Mahmood Kasbah <119080017+Mahmoodkasbah@users.noreply.github.com>
2026-07-30 12:14:23 +0300

    Update MainTabNavigator.js

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -6,7 +6,7 @@ import { COLORS, FONTS, TYPOGRAPHY, ICONS } from '../constants';
 
 import HomeScreen from '../screens/main/HomeScreen';
 import MyBookingsScreen from '../screens/main/MyBookingsScreen';
-import ComingSoonScreen from '../screens/ComingSoonScreen';
+import RoomsScreen from '../screens/main/RoomsScreen';
 import ProfileScreen from '../screens/main/ProfileScreen';
 
 const Tab = createBottomTabNavigator();
@@ -37,10 +37,9 @@ export default function MainTabNavigator() {
         },
         tabBarIcon: ({ focused, color, size }) => {
           const iconColor = focused ? COLORS.primary : COLORS.text.muted;
-          
           if (route.name === 'Home') return ICONS.home(size, iconColor);
           if (route.name === 'MyBookings') return ICONS.calendar(size, iconColor);
-          if (route.name === 'Explore') return ICONS.search(size, iconColor);
+          if (route.name === 'NewBooking') return ICONS.bed(size, iconColor);
           if (route.name === 'Profile') return ICONS.user(size, iconColor);
           
           return null;
@@ -48,7 +47,7 @@ export default function MainTabNavigator() {
       })}
     >
       <Tab.Screen name="Profile" component={ProfileScreen} options={{ tabBarLabel: 'حسابي' }} />
-      <Tab.Screen name="Explore" component={ComingSoonScreen} options={{ tabBarLabel: 'استكشف' }} />
+      <Tab.Screen name="NewBooking" component={RoomsScreen} options={{ tabBarLabel: 'حجز جديد' }} />
       <Tab.Screen name="MyBookings" component={MyBookingsScreen} options={{ tabBarLabel: 'حجوزاتي' }} />
       <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarLabel: 'الرئيسية' }} />
     </Tab.Navigator>
